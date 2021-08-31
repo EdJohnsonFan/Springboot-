b838919ae9c03730bee9d36bd61aa413497684ee
Johnson <fanw3648@Gmail.com>
2021-08-31 16:03:53 +0800

    8.31包名修改

---
+++ hospital-manage/src/main/java/com/johnson/hospital/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.johnson.hospital.mapper;
+
+import com.johnson.hospital.model.Schedule;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
